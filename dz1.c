e87c0c33bc5df1ec72a48d78aa47f17326a35f43
Uros Stojiljkovic <urosss007@gmail.com>
2018-03-24 13:43:49 +0100

    dz1 v2

--- dz1.c
+++ dz1.c
@@ -11,35 +11,33 @@ void printArray(char arr[], int n) {
 }
 
 void complement(char a[], int n) {
-	char t = 0;		//oznacava da li smo naisli na '1' sa desne strane
+	char t = 0;		// oznacava da li smo naisli na '1' sa desne strane
 	for (int i = n - 1; i >= 0; i--) {
-		if (!t) {
-			if (a[i]) {
-				t = 1;
+		if (!t) {	// ako jos nije bilo jedinice, nista se ne menja
+			if (a[i]) {		// ako je trenutno jedinica
+				t = 1;		// pamtimo da smo dosli do nje
 			}
 		}
-		else {
+		else {		// kad se prodje jedna jedinica, svi biti se komplementiraju
 			a[i] = (a[i]) ? 0 : 1;
 		}
 	}
 }
 
-//racuna vrednost celog broja
+// racuna vrednost celog broja, cija je predstava niz a
 int calculate(char a[], int n) {
 	int val = 0;
-	char b[MAX], c = 0;		//c ima vrednost 0 ako broj nije komplementiran, tj. ako je pozitivan
-	
-	for (int i = 0; i < n; i++) b[i] = a[i];
+	char c = 0;		// c ima vrednost 0 ako broj nije komplementiran, tj. ako je pozitivan
 
-	if (b[0]) {
-		complement(b, n);
+	if (a[0]) {
+		complement(a, n);
 		c = 1;
 		printf("%10s", "b_kompl: ");
-		printArray(b, n);
+		printArray(a, n);
 	}
 
 	for (int i = 0; i < n; i++) {
-		if (b[i]) {
+		if (a[i]) {
 			val += pow(2, (n - i - 1));
 		}
 	}
@@ -47,7 +45,7 @@ int calculate(char a[], int n) {
 	return c ? -val : val;
 }
 
-//formira niz dst dobijen pomeranjem niza src duzine n za k mesta ulevo
+// formira niz dst dobijen pomeranjem niza src duzine n za k mesta ulevo
 void shiftLeft(char src[], char dst[], int n, int k) {
 	for (int i = k; i < n; i++) {
 		dst[i - k] = src[i];
@@ -57,20 +55,20 @@ void shiftLeft(char src[], char dst[], int n, int k) {
 	}
 }
 
-//formira niz dst dobijen pomeranjem niza src duzine n za k mesta udesno
+// formira niz dst dobijen pomeranjem niza src duzine n za k mesta udesno
 void shiftRight(char arr[], char dst[], int n, int k) {
-	char c = arr[0];	//cifra koja se dodaje na k bitova najvece duzine, 0 za pozitivne brojeve, 1 za negativne
-	for (int i = n - k; i >= 0; i--) {
+	char c = arr[0];	// cifra koja se dodaje na k bitova najvece duzine, 0 za pozitivne brojeve, 1 za negativne
+	for (int i = n - k - 1; i >= 0; i--) {
 		dst[i + k] = arr[i];
 	}
-	for (int i = 0; i < k && i < n; i++) {		//uslov i < n, za slucaj da je k > n
+	for (int i = 0; i < k && i < n; i++) {		// uslov i < n, za slucaj da je k > n
 		dst[i] = c;
 	}
 }
 
 int main() {
-	char a[MAX], b[MAX];
-	int n, val;
+	char a[MAX], b[MAX];	// a je pocetni niz, b je novi niz dobijen pomeranjem niza a
+	int n, val, i, s;		// n - broj elemenata niza, val - vrednost broja cija je predstava niz b, s - broj mesta za koje se pomera
 
 	while (1) {
 		printf("Unesite duzinu niza: ");
@@ -78,18 +76,15 @@ int main() {
 
 		if (n <= 0 || n > MAX) break;
 
-		for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+		for (i = 0; i < n; i++) scanf("%d", &a[i]);
 
-		int s;
 		printf("Shift za: ");
 		scanf("%d", &s);
 
 		if (s > 0) {
-			//printf("shifting right... %d\n", s);
 			shiftRight(a, b, n, s);
 		}
 		else {
-			//printf("shifting left... %d\n", s);
 			shiftLeft(a, b, n, -s);
 		}
 		
